f910584f3374c8ca4b4e752571ca27e724c56e1a
Luana Lima Teixeira Alencar Sampai <a00987987@se14l0124-16.ad.bcit.ca>
2020-03-11 11:32:00 -0700

    changes on tutorial2

--- tutorial2/buttons/yes3.js
+++ tutorial2/buttons/yes3.js
@@ -3,16 +3,14 @@ var state=1;
 
 function border(){
 if (state === 1){
-document.getElementById("yes3").style.border = "thick solid #7558B7";
+document.getElementById("yes3").style.border = "2px solid #7558B7";
 document.getElementById("no3").style.border = "none";
 state = 2;
 }
 else {
-    document.getElementById("no3").style.border = "thick solid #7558B7";
+    document.getElementById("no3").style.border = "2px solid #7558B7";
     document.getElementById("yes3").style.border = "none";
     state = 1;
 }
 
     }
-
-
