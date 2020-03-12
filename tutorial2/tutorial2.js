310828b70d2033f3794b3cd1e2dbb6e09612471d
Luana Lima Teixeira Alencar Sampai <a00987987@se14l0121-06.ad.bcit.ca>
2020-03-12 14:29:59 -0700

    changes during meeting w henry

--- tutorial2/tutorial2.js
+++ tutorial2/tutorial2.js
@@ -15,7 +15,12 @@ function GoToNext(){
         document.querySelector("#button1").style.display="none";
         document.querySelector("#button2").style.display="none";
     } 
-   
+
+    else {
+        document.querySelector("#buttonscontainer").style.display="flex";
+        document.querySelector("#button1").style.display="flex";
+        document.querySelector("#button2").style.display="flex";
+    }
    
    
     if(tut_ind===2) {
@@ -23,10 +28,11 @@ function GoToNext(){
         document.querySelector("#rightwrong h1").innerText = "WRONG"
     }    
 
-
-
-  
-  
+    document.querySelector("#title").innerText = tut[tut_ind].title;
+    document.querySelector("#text1").innerText = tut[tut_ind].text1;
+    document.querySelector("#tut2image").src = tut[tut_ind].tut2image;
+    document.querySelector("#text2").innerText = tut[tut_ind].text2;
+    document.querySelector("#next_text").innerText = tut[tut_ind].next_text; 
 
 
 }
