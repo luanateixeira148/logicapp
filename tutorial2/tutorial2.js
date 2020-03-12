67da674f3b0efd667f12bd918ae3b2898a4662cd
Poone Ashja <you@example.com>
2020-03-12 12:36:42 -0700

    NOT WORKING

--- tutorial2/tutorial2.js
+++ tutorial2/tutorial2.js
@@ -7,14 +7,14 @@ document.querySelector("#next_text").innerText = tut[tut_ind].next_text;
 
 function GoToNext(){
     tut_ind++;
-    if(tut_ind===1 || tut_ind===4) {
-        document.querySelector("#rightwrong").style.display = "flex";
+    if(tut_ind===0) {
+        document.querySelector("#rightwrong").style.display = "none";
     }
     
-    if(tut_ind===0) {
-        document.querySelector("#buttonscontainer").style.display="flex";
-        document.querySelector("#button1").style.display="flex";
-        document.querySelector("#button2").style.display="flex";
+    if(tut_ind===1 || tut_ind===2 || tut_ind===4 || tut_ind===5 ) {
+        document.querySelector("#buttonscontainer").style.display="none";
+        document.querySelector("#button1").style.display="none";
+        document.querySelector("#button2").style.display="none";
     }
     document.querySelector("#title").innerText = tut[tut_ind].title;
     document.querySelector("#text1").innerText = tut[tut_ind].text1;
