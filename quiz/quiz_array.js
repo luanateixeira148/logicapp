2b021079b0df746cd4a9ea8e29ef6edeb66243bd
Luana Lima Teixeira Alencar Sampai <a00987987@se14l0121-06.ad.bcit.ca>
2020-03-12 13:54:48 -0700

    changes on js

--- quiz/quiz_array.js
+++ quiz/quiz_array.js
@@ -5,91 +5,57 @@ var quiz = [
         title: "Question 01",
         text: "The CANUCKS have LOST their last seven games. Thus, they will probably lose their next game.",
         quizimg: "imgs/hockey1.png",
-        // questioncontainer
-        // answer
         next_text:"Check Answer"
     },
     {
         title: "Question 01",
-        // progression
-        // rightwrong
         text: "The CANUCKS have LOST their last seven games. Thus, they will probably lose their next game.",
         quizimg: "imgs/hockey1.png",
-        // questioncontainer
-        // answer
         next_text:"Next Question"
     },
     {
         title: "Question 01",
-        // progression
-        // rightwrong
         text: "The CANUCKS have LOST their last seven games. Thus, they will probably lose their next game.",
         quizimg: "imgs/hockey1.png",
-        // questioncontainer
-        // answer
         next_text:"Next Question"
     },
 // question 02
     {
         title: "Question 02",
-        // progression
-        // rightwrong
         text: "All students go to school. You are a student. Therefore you go to school.",
         quizimg: "imgs/kidschool.png",
-        // questioncontainer
-        // answer
         next_text:"Check Answer"
     },
     {
         title: "Question 02",
-        // progression
-        // rightwrong
         text: "All students go to school. You are a student. Therefore you go to school.",
         quizimg: "imgs/kidschool.png",
-        // questioncontainer
-        // answer
         next_text:"Next Question"
     },
     {
         title: "Question 02",
-        // progression
-        // rightwrong
         text: "All students go to school. You are a student. Therefore you go to school.",
         quizimg: "imgs/kidschool.png",
-        // questioncontainer
-        // answer
         next_text:"Next Question"
     },
 
     // question 03
     {
         title: "Question 03",
-        // progression
-        // rightwrong
         text: "All birds can fly. An ostrich is a bird. Therefore an ostrich can fly.",
         quizimg: "imgs/ostrich.png",
-        // questioncontainer
-        // answer
         next_text:"Check Answer"
     },
     {
         title: "Question 03",
-        // progression
-        // rightwrong
         text: "All birds can fly. An ostrich is a bird. Therefore an ostrich can fly.",
         quizimg: "imgs/ostrich.png",
-        // questioncontainer
-        // answer
         next_text:"Check Result"
     },
     {
         title: "Question 03",
-        // progression
-        // rightwrong
         text: "All birds can fly. An ostrich is a bird. Therefore an ostrich can fly.",
         quizimg: "imgs/ostrich.png",
-        // questioncontainer
-        // answer
         next_text:"Check Result"
     }
 ]
--- quiz/quiz_functions.js
+++ quiz/quiz_functions.js
@@ -1,24 +1,31 @@
+document.querySelector("#title").innerText = quiz[quiz_ind].title;
+document.querySelector("#text").innerText = quiz[quiz_ind].text;
+document.querySelector("#quizimg").src = quiz[quiz_ind].quizimg;
+document.querySelector("#next_text").innerText = quiz[quiz_ind].next_text;
+
 function quizGoToNext() {
     quiz_ind++;
 
-    // progression bar
+    // progression bar and right/wrong
 
-    if(quiz_ind===1 || quiz_ind===2 || quiz_ind===4  || quiz_ind===5  || quiz_ind===7 || quiz_ind===8) {
-        document.querySelector("#progressionBar").style.display = "none";
-        document.querySelector("#rightwrong").style.display = "flex";
-    }
-    
-    if(quiz_ind===3) {
+        // show progression bar
+    if(quiz_ind===3 || quiz_ind===6) {
         document.querySelector("#progressionBar").style.display = "flex";
         document.querySelector("#rightwrong").style.display = "none";
     }
 
-    document.querySelector("#title").innerText = quiz[quiz_ind].title;
-    document.querySelector("#text").innerText = quiz[quiz_ind].text;
-    document.querySelector("#quizimg").src = quiz[quiz_ind].quizimg;
-    document.querySelector("#next_text").innerText = quiz[quiz_ind].next_text;
+        // show answer is right
+    if(quiz_ind===1 || quiz_ind===4 || quiz_ind===7) {
+        document.querySelector("#progressionBar").style.display = "none";
+        document.querySelector("#rightwrong").style.display = "flex";
+        document.querySelector("#rightwrong").style.backgroundColor = "#50C878";
+        document.querySelector("#rightwrong h1").innerText = "RIGHT!"
+    }
     
-    if(quiz_ind===2) {
+        // show answer is wrong
+    if(quiz_ind===2 || quiz_ind===5 || quiz_ind===8) {
+        document.querySelector("#progressionBar").style.display = "none";
+        document.querySelector("#rightwrong").style.display = "flex";
         document.querySelector("#rightwrong").style.backgroundColor = "#FA7070";
         document.querySelector("#rightwrong h1").innerText = "WRONG..."
     }
@@ -27,11 +34,12 @@ function quizGoToNext() {
 
     if(quiz_ind===1 || quiz_ind===2) {
         document.querySelector("#questioncontainer").style.display = "none";
-        document.querySelector("#bottomanswer").style.display = "bock";
+        document.querySelector("#bottomanswer").style.display = "flex";
+        document.querySelector("#bottomanswer h1").innerText = "INDUCTIVE REASONING"
     }
 
     if(quiz_ind===3) {
-        document.querySelector("#questioncontainer").style.display = "flex";
+        document.querySelector("#questioncontainer").style.display = "block";
         document.querySelector("#bottomanswer").style.display = "none";
     }
 }
@@ -40,4 +48,3 @@ document.querySelector("#title").innerText = quiz[quiz_ind].title;
     document.querySelector("#text").innerText = quiz[quiz_ind].text;
     document.querySelector("#quizimg").src = quiz[quiz_ind].quizimg;
     document.querySelector("#next_text").innerText = quiz[quiz_ind].next_text;
-
