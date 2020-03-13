bbbb9d0dd83d22e24f411406bc650b550416f684
Luana Lima Teixeira Alencar Sampai <a00987987@se14l0120-07.ad.bcit.ca>
2020-03-13 14:59:17 -0700

    organizing array

--- quiz/quiz_array.js
+++ quiz/quiz_array.js
@@ -1,42 +1,44 @@
 var quiz_ind = 0;
 var quiz = [
-// question 01
+
+    // question 01
     {
         title: "Question 01",
         text: "The CANUCKS have LOST their last seven games. Thus, they will probably lose their next game.",
         quizimg: "imgs/hockey1.png",
-        next_text:"Check Answer"
-    },
-    {
-        title: "Question 01",
-        text: "The CANUCKS have LOST their last seven games. Thus, they will probably lose their next game.",
-        quizimg: "imgs/hockey1.png",
-        next_text:"Next Question"
-    },
-    {
-        title: "Question 01",
-        text: "The CANUCKS have LOST their last seven games. Thus, they will probably lose their next game.",
-        quizimg: "imgs/hockey1.png",
-        next_text:"Next Question"
-    },
-// question 02
-    {
-        title: "Question 02",
-        text: "All students go to school. You are a student. Therefore you go to school.",
-        quizimg: "imgs/kidschool.png",
-        next_text:"Check Answer"
-    },
+        next_text:"Check Answer",
+        right: {
+            title: "Question 01",
+            text: "The CANUCKS have LOST their last seven games. Thus, they will probably lose their next game.",
+            quizimg: "imgs/hockey1.png",
+            next_text:"Next Question",
+        },
+        wrong: {
+            title: "Question 01",
+            text: "The CANUCKS have LOST their last seven games. Thus, they will probably lose their next game.",
+            quizimg: "imgs/hockey1.png",
+            next_text:"Next Question",
+        },
+    },    
+
+    // question 02
     {
         title: "Question 02",
         text: "All students go to school. You are a student. Therefore you go to school.",
         quizimg: "imgs/kidschool.png",
-        next_text:"Next Question"
-    },
-    {
-        title: "Question 02",
-        text: "All students go to school. You are a student. Therefore you go to school.",
-        quizimg: "imgs/kidschool.png",
-        next_text:"Next Question"
+        next_text:"Check Answer",
+        right: {
+            title: "Question 02",
+            text: "All students go to school. You are a student. Therefore you go to school.",
+            quizimg: "imgs/kidschool.png",
+            next_text:"Next Question",
+        },
+        wrong: {
+            title: "Question 02",
+            text: "All students go to school. You are a student. Therefore you go to school.",
+            quizimg: "imgs/kidschool.png",
+            next_text:"Next Question",
+        },
     },
 
     // question 03
@@ -44,18 +46,18 @@ var quiz = [
         title: "Question 03",
         text: "All birds can fly. An ostrich is a bird. Therefore an ostrich can fly.",
         quizimg: "imgs/ostrich.png",
-        next_text:"Check Answer"
-    },
-    {
-        title: "Question 03",
-        text: "All birds can fly. An ostrich is a bird. Therefore an ostrich can fly.",
-        quizimg: "imgs/ostrich.png",
-        next_text:"Check Result"
-    },
-    {
-        title: "Question 03",
-        text: "All birds can fly. An ostrich is a bird. Therefore an ostrich can fly.",
-        quizimg: "imgs/ostrich.png",
-        next_text:"Check Result"
+        next_text:"Check Answer",
+        right: {
+            title: "Question 03",
+            text: "All birds can fly. An ostrich is a bird. Therefore an ostrich can fly.",
+            quizimg: "imgs/ostrich.png",
+            next_text:"Check Result",
+        },
+        wrong: {
+            title: "Question 03",
+            text: "All birds can fly. An ostrich is a bird. Therefore an ostrich can fly.",
+            quizimg: "imgs/ostrich.png",
+            next_text:"Check Result",
+        }
     }
 ]
--- quiz/quiz_functions.js
+++ quiz/quiz_functions.js
@@ -3,51 +3,45 @@ document.querySelector("#text").innerText = quiz[quiz_ind].text;
 document.querySelector("#quizimg").src = quiz[quiz_ind].quizimg;
 document.querySelector("#next_text").innerText = quiz[quiz_ind].next_text;
 
+function quizSaveAnswer(n) {
+    quiz_answer = n;
+}
+
 function quizGoToNext() {
-    quiz_ind++;
-
-// progression bar and right/wrong
-
-        // show progression bar
-    if(quiz_ind===3 || quiz_ind===6) {
-        document.querySelector("#topbar").style.display = "flex";
-        document.querySelector("#rightwrong").style.display = "none";
-    }
-
-        // show answer is right
-    if(quiz_ind===1 || quiz_ind===4 || quiz_ind===7) {
+    console.log(quiz_answer)
+    if (quiz_answer === 1){
+        document.querySelector("#title").innerText = quiz[quiz_ind].right.title;
+        document.querySelector("#text").innerText = quiz[quiz_ind].right.text;
+        document.querySelector("#quizimg").src = quiz[quiz_ind].right.quizimg;
+        document.querySelector("#next_text").innerText = quiz[quiz_ind].right.next_text;
+        
         document.querySelector("#topbar").style.display = "none";
         document.querySelector("#rightwrong").style.display = "flex";
         document.querySelector("#rightwrong").style.backgroundColor = "#50C878";
         document.querySelector("#rightwrong h1").innerText = "RIGHT!"
+
+        document.querySelector("#questioncontainer").style.display = "none";
+        document.querySelector("#bottomanswer").style.display = "flex";
+        document.querySelector("#bottomanswer h1").innerText = "INDUCTIVE REASONING"
+        quiz_answer = 0;
     }
-    
-        // show answer is wrong
-    if(quiz_ind===2 || quiz_ind===5 || quiz_ind===8) {
+
+    else if (quiz_answer === 2) {
+        document.querySelector("#title").innerText = quiz[quiz_ind].wrong.title;
+        document.querySelector("#text").innerText = quiz[quiz_ind].wrong.text;
+        document.querySelector("#quizimg").src = quiz[quiz_ind].wrong.quizimg;
+        document.querySelector("#next_text").innerText = quiz[quiz_ind].wrong.next_text;
         document.querySelector("#topbar").style.display = "none";
         document.querySelector("#rightwrong").style.display = "flex";
         document.querySelector("#rightwrong").style.backgroundColor = "#FA7070";
         document.querySelector("#rightwrong h1").innerText = "WRONG..."
+        quiz_answer = 0;
     }
 
-// buttons
-
-        //show q1 answer
-    if(quiz_ind===1 || quiz_ind===2) {
-        document.querySelector("#questioncontainer").style.display = "none";
-        document.querySelector("#bottomanswer").style.display = "flex";
-        document.querySelector("#bottomanswer h1").innerText = "INDUCTIVE REASONING"
-    }
-
-        //show q2 and 3 answers
-    if(quiz_ind===4 || quiz_ind===5 || quiz_ind===7 || quiz_ind===8) {
-        document.querySelector("#questioncontainer").style.display = "none";
-        document.querySelector("#bottomanswer").style.display = "flex";
-        document.querySelector("#bottomanswer h1").innerText = "DEDUCTIVE REASONING"
-    }
-
-        //show option buttons
-    if(quiz_ind===3 || quiz_ind===6) {
+    else {
+        quiz_ind++;
+        document.querySelector("#topbar").style.display = "flex";
+        document.querySelector("#rightwrong").style.display = "none";
         document.querySelector("#questioncontainer").style.display = "block";
         document.querySelector("#bottomanswer").style.display = "none";
     }
@@ -58,13 +52,35 @@ function quizGoToNext() {
     document.querySelector("#next_text").innerText = quiz[quiz_ind].next_text;
 }
 
-// changing pages
-    function saveAnswer(n) {
-        quiz_answer = n;
-    }
 
 
 
+
+// buttons
+
+    //     //show q1 answer
+    // if(quiz_ind===1 || quiz_ind===2) {
+    //     document.querySelector("#questioncontainer").style.display = "none";
+    //     document.querySelector("#bottomanswer").style.display = "flex";
+    //     document.querySelector("#bottomanswer h1").innerText = "INDUCTIVE REASONING"
+    // }
+
+    //     //show q2 and 3 answers
+    // if(quiz_ind===4 || quiz_ind===5 || quiz_ind===7 || quiz_ind===8) {
+    //     document.querySelector("#questioncontainer").style.display = "none";
+    //     document.querySelector("#bottomanswer").style.display = "flex";
+    //     document.querySelector("#bottomanswer h1").innerText = "DEDUCTIVE REASONING"
+    // }
+
+        //show option buttons
+    // if(quiz_ind===3 || quiz_ind===6) {
+    //     document.querySelector("#questioncontainer").style.display = "block";
+    //     document.querySelector("#bottomanswer").style.display = "none";}
+
+// // changing pages
+//     function saveAnswer(n) {
+//         quiz_answer = n;
+//     }
 
 
 // function saveAnswer(n) {
@@ -76,3 +92,29 @@ function quizGoToNext() {
 // }
 
 // onclick: "saveAnswer(1)"
+
+
+
+// progression bar and right/wrong
+
+        // show progression bar
+    // if(quiz_ind===3 || quiz_ind===6) {
+    //     document.querySelector("#topbar").style.display = "flex";
+    //     document.querySelector("#rightwrong").style.display = "none";
+    // }
+
+    //     // show answer is right
+    // if(quiz_ind===1 || quiz_ind===4 || quiz_ind===7) {
+    //     document.querySelector("#topbar").style.display = "none";
+    //     document.querySelector("#rightwrong").style.display = "flex";
+    //     document.querySelector("#rightwrong").style.backgroundColor = "#50C878";
+    //     document.querySelector("#rightwrong h1").innerText = "RIGHT!"
+    // }
+    
+        // show answer is wrong
+    // if(quiz_ind===2 || quiz_ind===5 || quiz_ind===8) {
+    //     document.querySelector("#topbar").style.display = "none";
+    //     document.querySelector("#rightwrong").style.display = "flex";
+    //     document.querySelector("#rightwrong").style.backgroundColor = "#FA7070";
+    //     document.querySelector("#rightwrong h1").innerText = "WRONG..."
+    // }
