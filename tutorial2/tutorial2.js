08b3f2b84ce5516a1e12fd0f88f4516b80dfce20
Luana Lima Teixeira Alencar Sampai <a00987987@se14l0120-07.ad.bcit.ca>
2020-03-13 16:27:31 -0700

    tutorial 2

---
+++ tutorial2/tutorial2.js
@@ -0,0 +1,58 @@
+document.querySelector("#title").innerText = tut[tut_ind].title;
+document.querySelector("#text1").innerText = tut[tut_ind].text1;
+document.querySelector("#tut2image").src = tut[tut_ind].tut2image;
+document.querySelector("#text2").innerText = tut[tut_ind].text2;
+document.querySelector("#next_text").innerText = tut[tut_ind].next_text;
+
+function saveAnswer(n) {
+    tut_answer = n;
+}
+
+function GoToNext() {
+    console.log(tut_answer)
+    if (tut_answer === 1) {
+        //show correct text
+        document.querySelector("#title").innerText = tut[tut_ind].right.title;
+        document.querySelector("#text1").innerText = tut[tut_ind].right.text1;
+        document.querySelector("#tut2image").src = tut[tut_ind].right.tut2image;
+        document.querySelector("#text2").innerText = tut[tut_ind].right.text2;
+        document.querySelector("#next_text").innerText = tut[tut_ind].right.next_text;
+        document.querySelector("#rightwrong").style.display = "flex";
+            document.querySelector("#rightwrong").style.backgroundColor = "#50C878";
+            document.querySelector("#rightwrong h1").innerText = "RIGHT";
+            
+            document.querySelector("#buttonscontainer").style.display = "none";
+        tut_answer = 0;
+        console.log(tut_answer)
+    }
+
+    else if (tut_answer === 2) {
+        document.querySelector("#title").innerText = tut[tut_ind].wrong.title;
+        document.querySelector("#text1").innerText = tut[tut_ind].wrong.text1;
+        document.querySelector("#tut2image").src = tut[tut_ind].wrong.tut2image;
+        document.querySelector("#text2").innerText = tut[tut_ind].wrong.text2;
+        document.querySelector("#next_text").innerText = tut[tut_ind].wrong.next_text;
+        document.querySelector("#rightwrong").style.display = "flex";
+            document.querySelector("#rightwrong").style.backgroundColor = "#fa7070";
+            document.querySelector("#rightwrong h1").innerText = "WRONG";
+            
+            document.querySelector("#buttonscontainer").style.display = "none";
+        tut_answer = 0;
+        console.log(tut_answer)
+    }
+    else {
+        tut_ind++;
+        
+        document.querySelector("#buttonscontainer").style.display = "flex";
+        document.querySelector("#rightwrong").style.display = "none";
+        document.querySelector("#title").innerText = tut[tut_ind].title;
+        document.querySelector("#text1").innerText = tut[tut_ind].text1;
+        document.querySelector("#tut2image").src = tut[tut_ind].tut2image;
+        document.querySelector("#text2").innerText = tut[tut_ind].text2;
+        document.querySelector("#next_text").innerText = tut[tut_ind].next_text;
+
+
+    }
+
+}
+
