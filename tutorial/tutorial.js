98b0ab273c26583981e3c5de3923c381dd5ad5f9
= <=>
2020-03-13 14:10:52 -0700

    changes

--- tutorial/tutorial.js
+++ tutorial/tutorial.js
@@ -1,5 +1,9 @@
 
 function GoToNext() {
+    if (tut_ind >= 5) {
+        return false;
+    }
+
     tut_ind++;
     if(tut_ind === 5){
         document.querySelector("#ind1").style.display = "flex";
@@ -25,7 +29,7 @@ function GoToNext() {
 document.querySelector("#title").innerText = tut[tut_ind].title;
 document.querySelector("#text1").innerText = tut[tut_ind].text1;
 document.querySelector("#tutorialImage").src = tut[tut_ind].tutorialImage;
-document.querySelector("#text2").innerText = tut[tut_ind].text2;
+document.querySelector("#text2").innerText = tutw[tut_ind].text2;
 document.querySelector("#next_text").innerText = tut[tut_ind].next_text;
 document.querySelector("#next_icon").src = tut[tut_ind].next_icon;
 
