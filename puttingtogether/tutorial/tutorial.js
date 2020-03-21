688d85fe0dc38118b0201929c375d369f953f0c2
= <=>
2020-03-20 22:47:26 -0700

    front page

--- puttingtogether/tutorial/tutorial.js
+++ puttingtogether/tutorial/tutorial.js
@@ -2,7 +2,7 @@ var tut_ind = 0;
 document.querySelector("#title").innerText = tut[tut_ind].title;
 document.querySelector("#text1").innerText = tut[tut_ind].text1;
 document.querySelector("#tutorialImage").src = tut[tut_ind].tutorialImage;
-document.querySelector("#text2").innerText = tut[tut_ind].text2;
+document.querySelector("#text2").innerHTML = tut[tut_ind].text2;
 document.querySelector("#next_text").innerText = tut[tut_ind].next_text;
 document.querySelector("#next_icon").src = tut[tut_ind].next_icon;
 
@@ -18,7 +18,7 @@ function TutorialGoToNext() {
     document.querySelector("#title").innerText = tut[tut_ind].title;
     document.querySelector("#text1").innerText = tut[tut_ind].text1;
     document.querySelector("#tutorialImage").src = tut[tut_ind].tutorialImage;
-    document.querySelector("#text2").innerText = tut[tut_ind].text2;
+    document.querySelector("#text2").innerHTML = tut[tut_ind].text2;
     document.querySelector("#next_text").innerText = tut[tut_ind].next_text;
     document.querySelector("#next_icon").src = tut[tut_ind].next_icon;
 }
