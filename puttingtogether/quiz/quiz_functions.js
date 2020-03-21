e0911d354f25327b32985689b81822a039b18c16
= <=>
2020-03-20 22:43:20 -0700

    fixing stuff

--- puttingtogether/quiz/quiz_functions.js
+++ puttingtogether/quiz/quiz_functions.js
@@ -1,7 +1,7 @@
-document.querySelector("#title").innerText = quiz[quiz_ind].title;
-document.querySelector("#text").innerText = quiz[quiz_ind].text;
-document.querySelector("#quizimg").src = quiz[quiz_ind].quizimg;
-document.querySelector("#next_text").innerText = quiz[quiz_ind].next_text;
+// document.querySelector("#title").innerText = quiz[quiz_ind].title;
+// document.querySelector("#text").innerText = quiz[quiz_ind].text;
+// document.querySelector("#quizimg").src = quiz[quiz_ind].quizimg;
+// document.querySelector("#next_text").innerText = quiz[quiz_ind].next_text;
 
 function quizSaveAnswer(n) {
     quiz_answer = n;
