29ccc974b54acc56d9f6b4c95c01900b015d074e
= <=>
2020-03-20 23:42:33 -0700

    organizing

--- tutorial2/buttons/yes3.js
+++
@@ -1,9 +0,0 @@
-
-function border1(){
-        document.getElementById("button1").style.border = "2px solid #7558B7";
-        document.getElementById("button2").style.border = "none"; }
-
-function border2(){
-            document.getElementById("button2").style.border = "2px solid #7558B7";
-            document.getElementById("button1").style.border = "none"; }
-    
